a6433e7f6af21961fa2d8816f09a3dd1dbc01c37
Greg Medders <grmedders@gmail.com>
2018-07-29 22:57:57 -0400

    Fixed compiling with docker

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
 # OS image
-FROM gmedders/cpp_scientific_libraries:v0.1.2
+FROM gmedders/cpp_scientific_libraries:v0.1.3
 
 MAINTAINER gmedders "https://github.com/gmedders"
-
-# Define default command.
-CMD ["bash"]
 
 WORKDIR /app
 ADD . /app
 
 # Build the packaged
-RUN mkdir build && cd build && cmake .. && cmake --build .
+RUN mkdir build && cd build && cmake .. && make
 
 # When the image is run, go directly to the executables
 WORKDIR /app/build
--- thirdparty/picojson.cmake
+++ thirdparty/picojson.cmake
@@ -1,6 +1,6 @@
 set(PICOJSON_PREFIX kazuho-picojson-25fc213)
-set(PICOJSON_URL https://api.github.com/repos/kazuho/picojson/tarball/v1.3.0)
-set(PICOJSON_URL_MD5 b809f659a9036f93a8ca938dc386b47f)
+set(PICOJSON_URL https://github.com/kazuho/picojson/archive/v1.3.0.tar.gz)
+set(PICOJSON_URL_MD5 98cdf87f486c61794a4b02c4004cfb86)
 
 
 ExternalProject_Add(${PICOJSON_PREFIX}
@@ -10,7 +10,7 @@ ExternalProject_Add(${PICOJSON_PREFIX}
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     BUILD_IN_SOURCE 0
-    INSTALL_COMMAND mkdir ../../../deps/include/picojson && cp ../kazuho-picojson-25fc213/picojson.h ../../../deps/include/picojson
+    INSTALL_COMMAND mkdir -p ../../../deps/include/picojson && cp ../kazuho-picojson-25fc213/picojson.h ../../../deps/include/picojson
     LOG_DOWNLOAD 1
     LOG_BUILD 1
 )
