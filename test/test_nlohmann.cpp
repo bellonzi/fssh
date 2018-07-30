8b5c20e9395455953a2d2a4c51439b62ecdc0cec
Greg Medders <grmedders@gmail.com>
2018-07-30 00:02:44 -0400

    Added examples for nlohmann/json

--- test/test_nlohmann.cpp
+++ test/test_nlohmann.cpp
@@ -12,3 +12,24 @@ TEST(nlohmann_json, read_string_directly) {
   EXPECT_TRUE(j["type"] == std::string("attempt at json"));
   EXPECT_TRUE(j["lunch"] == "spam");
 }
+
+TEST(nlohmann_json, read_string_from_file) {
+
+  std::ifstream file("../test/stringify_example.json");
+  nlohmann::json j;
+  file >> j;
+  EXPECT_TRUE(j["type"] == std::string("attempt at json"));
+  EXPECT_TRUE(j["lunch"] == "spam");
+}
+
+TEST(nlohmann_json, read_numbers_from_file) {
+
+  std::ifstream file("../test/numeric_example.json");
+  nlohmann::json j;
+  file >> j;
+  EXPECT_EQ(j["nsteps"], 100);
+  EXPECT_DOUBLE_EQ(j["x"][0], 0.1);
+  EXPECT_DOUBLE_EQ(j["x"][1], 1.0);
+  EXPECT_DOUBLE_EQ(j["p"][0], -1.0);
+  EXPECT_DOUBLE_EQ(j["p"][1], 30.0);
+}
