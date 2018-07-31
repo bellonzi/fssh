f9d3cd22eeee18f83cea23ac73d2d4e222ef7525
bellonzi <bellonzi@sas.upenn.edu>
2018-07-31 16:28:10 -0400

    Hamil Unit Tests Pass

--- test/test_hamil.cpp
+++ test/test_hamil.cpp
@@ -18,33 +18,35 @@ TEST(hamil, set_params) {
 // Uncomment these expects, then run the tests. Each expect will fail and
 // tell you what the value should have been. Then copy that number below
 
-// TEST(hamil, HamilD) {
-//   params config;
-//   config.read_input("../test/reference_config.json");
-//
-//   hamil curr_H;
-//   curr_H.set_params(config);
-//
-//   std::mt19937 prng;
-//   prng.seed(config.seed);
-//
-//   traj curr_traj;
-//   curr_traj.initial(config, prng);
-//
-//   curr_H.HamilA(config, curr_traj);
-//
-//   EXPECT_NEAR(curr_H.Ve(0, 0), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Ve(1, 1), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Ve(0, 1), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Ve(1, 0), 0.0, 1.0E-8);
-//
-//   EXPECT_NEAR(curr_H.Fe(0, 0, 0), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Fe(1, 1, 0), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Fe(0, 1, 0), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Fe(1, 0, 0), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Fe(0, 1, 1), 0.0, 1.0E-8);
-//   EXPECT_NEAR(curr_H.Fe(1, 0, 1), 0.0, 1.0E-8);
-// }
+TEST(hamil, HamilD) {
+  params config;
+  config.read_input("../test/reference_config.json");
+
+  hamil curr_H;
+  curr_H.set_params(config);
+
+  std::mt19937 prng;
+  prng.seed(config.seed);
+
+  traj curr_traj;
+  curr_traj.initial(config, prng);
+
+  curr_H.HamilA(config, curr_traj);
+
+  EXPECT_NEAR(curr_H.Ve(0, 0).real(), 0.094172360229944088, 1.0E-8);
+  EXPECT_NEAR(curr_H.Ve(1, 1).real(), -0.094172360229944088, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Ve(0, 1)), 0.033638765862047407, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Ve(1, 0)), 0.033638765862047407, 1.0E-8);
+
+  EXPECT_NEAR(abs(curr_H.Fe(0, 0, 0)), 0.33807452152944378, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(1, 1, 0)), 0.33807452152944378, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(0, 1, 0)), 0.94644600686605007, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(1, 0, 0)), 0.94644600686605007, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(0, 0, 1)), 0.0, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(1, 1, 1)), 0.0, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(0, 1, 1)), 0.010091629758614221, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Fe(1, 0, 1)), 0.010091629758614221, 1.0E-8);
+}
 
 TEST(hamil, HamilA) {
   params config;
@@ -66,13 +68,15 @@ TEST(hamil, HamilA) {
 
   curr_H.HamilA(config, curr_traj);
 
-  // EXPECT_NEAR(curr_H.eigs(0), 0.0, 1.0E-8);
-  // EXPECT_NEAR(curr_H.eigs(1), 0.0, 1.0E-8);
-  //
-  // EXPECT_NEAR(curr_H.Ue(0, 0), 0.0, 1.0E-8);
-  // EXPECT_NEAR(curr_H.Ue(1, 1), 0.0, 1.0E-8);
-  // EXPECT_NEAR(curr_H.Ue(0, 1), 0.0, 1.0E-8);
-  // EXPECT_NEAR(curr_H.Ue(1, 0), 0.0, 1.0E-8);
+  EXPECT_NEAR(curr_H.eigs(0),-0.1, 1.0E-8);
+  EXPECT_NEAR(curr_H.eigs(1), 0.1, 1.0E-8);
+  EXPECT_NEAR(curr_H.eigs(0),-config.jA, 1.0E-8);
+  EXPECT_NEAR(curr_H.eigs(1), config.jA, 1.0E-8);
+
+  EXPECT_NEAR(abs(curr_H.Ue(0, 0)), 0.17069914718673779, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Ue(0, 1)), 0.98532319629130838, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Ue(1, 0)), 0.98532319629130838, 1.0E-8);
+  EXPECT_NEAR(abs(curr_H.Ue(1, 1)), 0.17069914718673779, 1.0E-8);
 
   for (int i = 0; i < config.cdim; ++i) {
     EXPECT_DOUBLE_EQ(curr_traj.F(i), 0.0);
