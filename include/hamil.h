e3ccdf547e5bf950d502ff1e2033a23b26219ce6
bellonzi <bellonzi@sas.upenn.edu>
2018-08-06 23:13:32 -0400

    removed rk4 cases for Hamil

--- include/hamil.h
+++ include/hamil.h
@@ -19,10 +19,6 @@ struct hamil {
   void HamilA(const params &, traj &curr_traj);
   void set_params(const params &);
 
-  //update Hamil for RK4
-  void HamilD(const params &, traj &curr_traj, rk4 &);
-  void HamilA(const params &, traj &curr_traj, rk4 &);
-
   double jA, jlx, jbx; // Defines Hamil in Joe's code
 };
 
