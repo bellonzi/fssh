09537d552570b82896bd1ae497be1fc97ef3615f
bellonzi <bellonzi@sas.upenn.edu>
2018-08-07 16:23:35 -0400

    Added quantum DOF to RK4

--- src/fssh.cpp
+++ src/fssh.cpp
@@ -32,27 +32,34 @@ void propagate(const params &config, traj &curr_traj, hamil &curr_H) {
 
   // k_{1}=0.5 dt f(t_{n}, y_{n})
   curr_H.HamilA(curr_traj);
+  update_dcdt(dcdt, curr_traj, curr_H);
   k1.x = config.dt2 * curr_traj.p;
   k1.p = config.dt2 * curr_H.F;
-  // k1.psi = config.dt2 * dcdt;
+  k1.psi = config.dt2 * dcdt;
 
   // k_{2}=0.5 dt f(t_{n} + dt/2, y_{n} + k_{1})
   temp_traj = add_rk4(curr_traj, k1);
   curr_H.HamilA(temp_traj);
+  update_dcdt(dcdt, temp_traj, curr_H);
   k2.x = config.dt2 * temp_traj.p;
   k2.p = config.dt2 * curr_H.F;
+  k2.psi = config.dt2 * dcdt;
 
   // k_{3}=dt f(t_{n} + dt/2, y_{n} + k_{2})
   temp_traj = add_rk4(curr_traj, k2);
   curr_H.HamilA(temp_traj);
+  update_dcdt(dcdt, temp_traj, curr_H);
   k3.x = config.dt * temp_traj.p;
   k3.p = config.dt * curr_H.F;
+  k3.psi = config.dt * dcdt;
 
   // k_{4}=dt f(t_{n} + dt, y_{n} + k_{3})
   temp_traj = add_rk4(curr_traj, k3);
   curr_H.HamilA(temp_traj);
+  update_dcdt(dcdt, temp_traj, curr_H);
   k4.x = config.dt * temp_traj.p;
   k4.p = config.dt * curr_H.F;
+  k4.psi = config.dt * dcdt;
 
   // y_{n+1}= y_{n} + 1/6 (2 k_{1} + 4 k_{2} + 2 k_{3} + k_{4})
   curr_traj.x = curr_traj.x + (2 * k1.x + 4 * k2.x + 2 * k3.x + k4.x) / 6;
