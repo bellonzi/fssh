b759e87afba60a94469aa6eecc17f44e83b6034d
bellonzi <bellonzi@sas.upenn.edu>
2018-08-07 16:00:22 -0400

    Updated to reflect new Hamil structure and add dcdt calculation

--- src/fssh.cpp
+++ src/fssh.cpp
@@ -6,46 +6,51 @@
 
 namespace fssh {
 
+void update_dcdt(arma::cx_vec &dcdt, const traj &curr_traj,
+                 const hamil &curr_H) {
+  dcdt.zeros();
+  arma::cx_vec temp_dij;
+  temp_dij.zeros(curr_H.cdim);
+  for (int i = 0; i < curr_H.qdim; i++) {
+    dcdt(i) -= arma::cx_double(0, 1) * curr_traj.psi(i) * curr_H.eigs(i);
+    for (int j = 0; j < curr_H.qdim; j++) {
+      if (j != i) {
+        temp_dij = curr_H.dij(arma::span(i), arma::span(j), arma::span::all);
+        dcdt(i) -= curr_traj.psi(j) * dot(curr_traj.p, temp_dij);
+      }
+    }
+  }
+  return;
+}
+
 void propagate(const params &config, traj &curr_traj, hamil &curr_H) {
   // t_{n+1}= t_{n} + dt
   rk4 k1(config), k2(config), k3(config), k4(config);
   traj temp_traj;
   temp_traj.initial_zero(config);
-  arma::cx_vec dcdt(config.qdim); 
+  arma::cx_vec dcdt(curr_H.qdim);
 
   // k_{1}=0.5 dt f(t_{n}, y_{n})
-  curr_H.HamilA(config, curr_traj);
-  k1.x =   config.dt2 * curr_traj.p;
-  k1.p =   config.dt2 * curr_H.F;
-
-  dcdt.zeros();
-  for(int i = 0; i<config.qdim; i++){
-     dcdt(i) -= arma::cx_double(0,1)*curr_traj.psi(i)*curr_H.eigs(i);
-     for(int j = 0; j<config.qdim; j++){
-        if(j!=i){
-          arma::cx_vec temp_dij = curr_H.dij(arma::span(i),arma::span(j),arma::span::all);
-          dcdt(i) -= curr_traj.psi(j)*dot(curr_traj.p,temp_dij);
-     }
-     }
-  }
-
-  //k1.psi = config.dt2 * dcdt;
+  curr_H.HamilA(curr_traj);
+  k1.x = config.dt2 * curr_traj.p;
+  k1.p = config.dt2 * curr_H.F;
+  // k1.psi = config.dt2 * dcdt;
 
   // k_{2}=0.5 dt f(t_{n} + dt/2, y_{n} + k_{1})
   temp_traj = add_rk4(curr_traj, k1);
-  curr_H.HamilA(config, temp_traj);
+  curr_H.HamilA(temp_traj);
   k2.x = config.dt2 * temp_traj.p;
   k2.p = config.dt2 * curr_H.F;
 
   // k_{3}=dt f(t_{n} + dt/2, y_{n} + k_{2})
   temp_traj = add_rk4(curr_traj, k2);
-  curr_H.HamilA(config, temp_traj);
+  curr_H.HamilA(temp_traj);
   k3.x = config.dt * temp_traj.p;
   k3.p = config.dt * curr_H.F;
 
   // k_{4}=dt f(t_{n} + dt, y_{n} + k_{3})
   temp_traj = add_rk4(curr_traj, k3);
-  curr_H.HamilA(config, temp_traj);
+  curr_H.HamilA(temp_traj);
   k4.x = config.dt * temp_traj.p;
   k4.p = config.dt * curr_H.F;
 
@@ -55,7 +60,7 @@ void propagate(const params &config, traj &curr_traj, hamil &curr_H) {
   curr_traj.psi =
       curr_traj.psi + (2 * k1.psi + 4 * k2.psi + 2 * k3.psi + k4.psi) / 6;
 
-  curr_H.HamilA(config, curr_traj);
+  curr_H.HamilA(curr_traj);
 
   // call hop_or_not
 
@@ -86,7 +91,7 @@ int main() {
 
     traj curr_traj;
     curr_traj.initial(config, prng);
-    curr_H.HamilA(config, curr_traj);
+    curr_H.HamilA(curr_traj);
 
     for (int istep = 0; istep < config.nsteps; istep++) {
       fssh::propagate(config, curr_traj, curr_H);
